685c504dcea29f0f6a774aafdfd1aed013310508
learner-Roshan <161870023+learner-Roshan@users.noreply.github.com>
2024-06-08 05:57:40 +0000

    scope learning

--- 01_basics/scopes.js
+++ 01_basics/scopes.js
@@ -1,1 +1,15 @@
 //   {}  curly parenthesis is SCOPE!
+
+
+console.log(addone(1));         //this gives output even when called before function defination since function addOne is not stored in variable
+function addone(num){
+    return num +1
+}
+
+console.log(addtwo(1));         //this aint valid as addTwo is a variable and is defined below calling
+const addtwo = function(num){
+    return num + 2;
+}
+console.log(addtwo(1));
+
+//this is mini hoisting
